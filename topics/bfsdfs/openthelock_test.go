6cac99bfcf7f0292234092414181d27d54822bb8
Yehuda Makarov <yehudamakarov@gmail.com>
2020-11-03 23:43:31 -0500

    openthelock.go solution. but why the if check in the noted todo?

---
+++ topics/bfsdfs/openthelock_test.go
@@ -0,0 +1,18 @@
+package bfsdfs
+
+import "testing"
+
+// Input: deadends = ["0201","0101","0102","1212","2002"], target = "0202"
+
+// ["8887","8889","8878","8898","8788","8988","7888","9888"]
+// "8888"
+func TestOpenLock(t *testing.T) {
+	// got := OpenLock([]string{"0201", "0101", "0102", "1212", "2002"}, "0202")
+	// expected := 6
+
+	got := OpenLock([]string{"8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"}, "8888")
+	expected := -1
+	if got != expected {
+		t.Errorf("Got: %d, expected: %d ", got, expected)
+	}
+}
